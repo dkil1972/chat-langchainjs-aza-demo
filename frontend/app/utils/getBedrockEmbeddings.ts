257113544fe82ca6c7ec165e29ccb790e3938664
dermot <dermot@azafinance.com>
2024-08-30 14:59:54 +0100

    getting credentials from env variables

--- frontend/app/utils/getBedrockEmbeddings.ts
+++ frontend/app/utils/getBedrockEmbeddings.ts
@@ -15,14 +15,17 @@ export async function getBedrockEmbeddings() {
   // });
 
   // const credentials = await Auth.currentCredentials();
+  console.log("access key id", process.env.AWS_ACCESS_KEY_ID);
+  console.log("secret access key", process.env.AWS_SECRET_ACCESS_KEY);
+  console.log("session token", process.env.AWS_SESSION_TOKEN);
 
   return new BedrockEmbeddings({
     model: "amazon.titan-embed-text-v2:0",
     region: "eu-west-2",
     credentials: {
-      accessKeyId: "",
-      secretAccessKey: "",
-      sessionToken: "",
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      sessionToken: process.env.AWS_SESSION_TOKEN!,
     },
   });
 }
